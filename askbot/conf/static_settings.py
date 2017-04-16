37442cc26de280a7f4e701c151c5db5df9e9a41c
Eric <erichuang1994@users.noreply.github.com>
2017-04-16 17:26:30 +0800

    Fix ASKBOT_TRANSLATE_URL (#3)
    * fix ASKBOT_TRANSLATE_URL
    
    * remove print and set TRANSLATE_URL back to True(But this vari seems assigned but never userd)
    
    * Update static_settings.py
    
    SET TRANSLATE_URL = settings.ASKBOT_TRANSLATE_URL avoid hard code.

--- askbot/conf/static_settings.py
+++ askbot/conf/static_settings.py
@@ -1,5 +1,8 @@
 from django.conf import settings
-from django.utils.translation import pgettext
+if settings.ASKBOT_TRANSLATE_URL == True:
+    from django.utils.translation import pgettext
+else:
+    pgettext = lambda context, value: value
 from appconf import AppConf
 import os
 
@@ -30,7 +33,7 @@ class AskbotStaticSettings(AppConf):
     QUESTION_PAGE_BASE_URL = pgettext('urls', 'question') + '/'
     SERVICE_URL_PREFIX = 's/' # prefix for non-UI urls
     SELF_TEST = True # if true - run startup self-test
-    TRANSLATE_URL = True # set true to localize urls
+    TRANSLATE_URL = settings.ASKBOT_TRANSLATE_URL # set true to localize urls
     WHITELISTED_IPS = tuple() # a tuple of whitelisted ips for moderation
 
     class Meta:
