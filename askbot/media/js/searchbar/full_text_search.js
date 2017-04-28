00364bef4b245184bfc168f9f06d2043175207bd
ipcjs <gipcjs@gmail.com>
2017-04-29 01:19:23 +0800

    When you delete a tag, the search box does not automatically get focus. (see: #12)

--- askbot/media/js/searchbar/full_text_search.js
+++ askbot/media/js/searchbar/full_text_search.js
@@ -237,8 +237,6 @@ FullTextSearch.prototype.renderFullTextSearchResult = function (data) {
         askHrefBase + data.query_data.ask_query_string
     ); /* INFO: ask_query_string should already be URL-encoded! */
 
-    this._query.focus();
-
     var old_list = $('#' + this._q_list_sel);
     var new_list = $('<div></div>').hide().html(data.questions);
     new_list.find('.timeago').timeago();
